be61fc4bd2a1d50c2ad32c28a687c954dbf51b5d
Yasmin Behi <ybehi0001@yahoo.com>
2019-04-17 10:27:25 -0400

    iniitial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Enter your name : ");
+        Scanner keyboard = new Scanner(System.in);
+        String name = keyboard.nextLine();
+	System.out.println("Hello, " + name + "!");
+	System.out.println("Today is April 17th, 2019");
+
+
+    }
+}
